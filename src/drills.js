abdcff36a5fb50b61d715466276703ff34743217
AmyBraswell <braswellamy@mac.com>
2020-04-10 15:53:55 -0500

    broken before all hook

---
+++ src/drills.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL,
+})
+
+function searchByItemName(searchTerm) {
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+      console.log('SEARCH TERM', { searchTerm })
+      console.log(result)
+    })
+}
+
+searchByItemName('urger')
---
+++ test/setup.js
@@ -0,0 +1,12 @@
+// SHOPPING-LIST
+  
+require('dotenv').config();
+global.expect = require('chai').expect;
+
+
+// AMAZOG PROJECT
+// const {expect} = require('chai')
+// const supertest = require('supertest')
+
+// global.expect = expect
+// global.supertest = supertest
